ebcd22a768c13119e1971ab1d8611e9e5831dc52
westkite2 <westkite2@gmail.com>
2022-05-23 21:19:48 +0900

    Add boolean 2

---
+++ Boolean2/boolean1B_tb.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/10/12 15:25:21
+// Design Name: 
+// Module Name: boolean1B_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module boolean1B_tb;
+
+reg A, B, C;
+wire D;
+
+boolean1B u_bool1B(
+.a (A ),
+.b (B ),
+.c (C ),
+.d (D ) );
+
+initial A = 0;
+always A = #400 ~A;
+
+initial B = 0;
+always B = #200 ~B;
+
+initial C = 0;
+always C = #100 ~C;
+
+initial begin
+    #800
+    $finish;
+end
+
+endmodule
+
